667c79355974e0e57ce553dde57efa1a57d84263
Yushi-Goto <g.yushi1958@gmail.com>
2020-07-21 04:06:15 +0900

    Add class:AddNoise

--- util/transforms.py
+++ util/transforms.py
@@ -1,7 +1,29 @@
 import torch
-import numpy as np
+import numpy
 import cv2
 
+class Resize(object):
+    """Resize a face image to minimum size"""
+    def __init__(self, output_size, inC):
+        self.output_size = output_size
+        if inC == 1:
+            self.grayscale = True
+
+    def __call__(self, img):
+        if not (self.output_size == None):
+            img = cv2.resize(img, (self.output_size, self.output_size))
+        if self.grayscale:
+            img = numpy.reshape(img,(img.shape[0], img.shape[1], 1))
+        return img
+
+class AddNoise(object):
+    """Add noise to teach img for cleate target img"""
+    def __init__(self, sigma):
+        self.sigma = sigma
+
+    def __call__(self, teach_img):
+        target_img = teach_img + (self.sigma / 255) * numpy.random.randn(*teach_img.shape)
+        return {'teach_img':teach_img, 'target_img':target_img}
 
 class ToTensor(object):
     """Convert ndarrays in sample to Tensors."""
@@ -21,22 +43,22 @@ def func_rf(img, p):
     if p==0:
         img=img
     elif p==1:
-        img=np.flip(img, axis=1)
+        img=numpy.flip(img, axis=1)
     elif p==2:
-        img=np.rot90(img, k=1, axes=(0,1))
+        img=numpy.rot90(img, k=1, axes=(0,1))
     elif p==3:
-        img=np.flip(img, axis=1)
-        img=np.rot90(img, k=1, axes=(0,1))
+        img=numpy.flip(img, axis=1)
+        img=numpy.rot90(img, k=1, axes=(0,1))
     elif p==4:
-        img=np.rot90(img, k=2, axes=(0,1))
+        img=numpy.rot90(img, k=2, axes=(0,1))
     elif p==5:
-        img=np.flip(img, axis=1)
-        img=np.rot90(img, k=2, axes=(0,1))
+        img=numpy.flip(img, axis=1)
+        img=numpy.rot90(img, k=2, axes=(0,1))
     elif p==6:
-        img=np.rot90(img, k=3, axes=(0,1))
+        img=numpy.rot90(img, k=3, axes=(0,1))
     elif p==7:
-        img=np.flip(img, axis=1)
-        img=np.rot90(img, k=3, axes=(0,1))
+        img=numpy.flip(img, axis=1)
+        img=numpy.rot90(img, k=3, axes=(0,1))
 
     img=img.copy()
 
