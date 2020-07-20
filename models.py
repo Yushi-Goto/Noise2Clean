6fe4a3ad6edf012cd1015920d96b5e906229ba3f
Yushi-Goto <g.yushi1958@gmail.com>
2020-07-21 04:01:46 +0900

    Add method to overwrite conv param

--- models.py
+++ models.py
@@ -1,15 +1,18 @@
 import os
 import numpy
+from matplotlib import pylab as plt
 import torch
 import torch.nn as nn
+from torchvision import transforms
+from torch import optim
+import cv2
 import datasets
-import torch import optim
 import nets
-import import utils.transforms
+import util.transforms
 
 
 class N2CModel(object):
-    def __init__(self, params):
+    def __init__(self, param):
         super(N2CModel, self).__init__()
         self.device = param['device']
         self.model_path = param['model_path']
@@ -17,12 +20,19 @@ class N2CModel(object):
         if param['mode'] == 'train':
             self.batch_size = param['batch_size']
 
-            self.train_data_transform = transforms.Compose([
-                util.transforms.RotateFlip(),
-                util.transforms.ToTensor()])
+            if param['lpfir']:
+                self.train_data_transform = transforms.Compose([
+                    util.transforms.Resize(param['img_size'], param['inC']),
+                    util.transforms.AddNoise(param['sigma']),
+                    util.transforms.ToTensor()])
+            else:
+                self.train_data_transform = transforms.Compose([
+                    util.transforms.Resize(param['img_size'], param['inC']),
+                    util.transforms.AddNoise(param['sigma']),
+                    util.transforms.RotateFlip(),
+                    util.transforms.ToTensor()])
 
-            self.train_dataset = datasets.N2CDataset(dataset_path=param['dataset_path'], mode=param['train'],
-                                                        inC=param['inC'], sigma=param['sigma'], transform=self.train_data_transform)
+            self.train_dataset = datasets.N2CDataset(dataset_path=param['dataset_path'], mode=param['mode'], transform=self.train_data_transform)
             self.train_loader = torch.utils.data.DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True)
 
             self.net = nets.UNet(inC=param['inC'], midC=param['midC']).to(param['device'])
@@ -31,16 +41,15 @@ class N2CModel(object):
             self.optimizer = optim.Adam(self.net.parameters(), param['lr'])
             self.criterion = nn.MSELoss()
 
-            self.save_train_parameters()
-
         elif param['mode'] == 'test':
             self.batch_size = param['batch_size']
 
             self.test_data_transform = transforms.Compose([
+                util.transforms.Resize(param['img_size'], param['inC']),
+                util.transforms.AddNoise(param['sigma']),
                 util.transforms.ToTensor()])
 
-            self.test_dataset = datasets.N2CDataset(dataset_path=param['dataset_path'], mode=param['test'],
-                                                        inC=param['inC'], sigma=param['sigma'], transform=self.test_data_transform)
+            self.test_dataset = datasets.N2CDataset(dataset_path=param['dataset_path'], mode=param['mode'], transform=self.test_data_transform)
             self.test_loader = torch.utils.data.DataLoader(self.test_dataset, batch_size=self.batch_size)
 
             self.net = nets.UNet(inC=param['inC'], midC=param['midC']).to(param['device'])
@@ -49,14 +58,17 @@ class N2CModel(object):
 
         elif param['mode'] == 'run':
             self.run_data_transform = transforms.Compose([
+                util.transforms.Resize(None, param['inC']),
+                util.transforms.AddNoise(param['sigma']),
                 util.transforms.ToTensor()])
 
-            self.run_dataset = datasets.N2CDataset(dataset_path=param['dataset_path'], mode=param['run'],
-                                                    sigma=param['sigma'], transform=self.run_data_transform, data_path=param['data_path'])
+            self.run_dataset = datasets.N2CDataset(dataset_path=param['dataset_path'], mode=param['mode'],
+                                                    transform=self.run_data_transform, data_path=param['data_path'])
+            self.run_loader = torch.utils.data.DataLoader(self.run_dataset, batch_size=len(self.run_dataset))
 
             self.net = nets.UNet(inC=param['inC'], midC=param['midC']).to(param['device'])
 
-    def train(self, epochs, fir):
+    def train(self, epochs, lpfir):
         self.net.train()
 
         train_loss_list = []
@@ -68,28 +80,28 @@ class N2CModel(object):
 
                 self.optimizer.zero_grad()
                 if (e == 0) and (batch_idx == 0):
-                    if fir:
-                        self.selected_key()
+                    if lpfir:
+                        self.select_param_conv()
                         self.over_write_forward()
                 batch_output = self.net(batch_target)
                 loss = self.criterion(batch_output, batch_teach)
                 train_loss += loss.item()
                 loss.backward()
-                if fir:
+                if lpfir:
                     self.over_write_backward()
                 self.optimizer.step()
             train_loss_list.append(train_loss / len(self.train_dataset))
 
         self.save_model()
-        self.plot_loss()
+        self.plot_loss(epochs, train_loss_list)
         print('finished training')
-        print('last trainning loss : {}'.format(train_loss_list[-1])
+        print('last trainning loss : {}'.format(train_loss_list[-1]))
 
     def test(self):
         self.load_model()
         self.net.eval()
         with torch.no_grad():
-            test_loss
+            test_loss = 0
             for batch_idx, sample in enumerate(self.test_loader):
                 batch_target = sample['target_img'].to(self.device)
                 batch_teach  =sample['teach_img'].to(self.device)
@@ -106,7 +118,7 @@ class N2CModel(object):
         self.load_model()
         self.net.eval()
         with torch.no_grad():
-            sample = self.run_dataset[0]
+            sample = next(self.run_loader.__iter__())
             target = sample['target_img'].to(self.device)
             teach = sample['teach_img'].to(self.device)
 
@@ -123,17 +135,23 @@ class N2CModel(object):
         """Load model from self.model_path"""
         self.net.load_state_dict(torch.load(self.model_path, map_location=self.device))
 
-    def plot_loss(self):
-        pass
+    def plot_loss(self, epochs, loss_list):
+        plt.figure()
+        plt.plot(range(epochs), loss_list, color='blue', linestyle='-', label='train_loss')
+        plt.legend()
+        plt.xlabel('epoch')
+        plt.ylabel('loss')
+        plt.title('Train Loss')
+        plt.savefig('TrainLossFig.png')
 
     def save_img(self, target, teach, output):
         target_img = numpy.squeeze(target.to(torch.device('cpu')).numpy())
-        teach_img = numpy.squeeze(teach.to(torch.device('cpy')).numpy())
-        output_img = numpy.squeeze(output.to(torch.device('cpy')).numpy())
+        teach_img = numpy.squeeze(teach.to(torch.device('cpu')).numpy())
+        output_img = numpy.squeeze(output.to(torch.device('cpu')).numpy())
 
-        cv2.imwrite('target_img.jpg', target_img)
-        cv2.imwrite('teach_img.jpg', teach_img)
-        cv2.imwrite('output_img', output_img)
+        cv2.imwrite('target_img.jpg', target_img * 255)
+        cv2.imwrite('teach_img.jpg', teach_img * 255)
+        cv2.imwrite('output_img.jpg', output_img * 255)
 
     def over_write_forward(self):
         param_dic = self.net.state_dict()
@@ -182,9 +200,11 @@ class N2CModel(object):
                     p.grad[:, :, radius, 0+x] = sum_grad
                     p.grad[:, :, radius, -1-x] = sum_grad
 
-    def select_weight(self):
+    def select_param_conv(self):
         param_dic = self.net.state_dict()
         self.param_conv = []
         for name in param_dic.keys():
             if not (('bias' in name) or ('bn' in name) or ('deconv' in name)):
                 self.param_conv.append(name)
+        print(param_dic.keys())
+        print(self.param_conv)
